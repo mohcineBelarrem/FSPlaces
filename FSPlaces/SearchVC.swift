fbf03b73b540e63b232362524954a6a90b3d6d23
Mohcine Belarrem <belarrem@idm.fr>
2015-07-27 09:35:23 +0200

    UiElements set

--- FSPlaces/SearchVC.swift
+++ FSPlaces/SearchVC.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class SerachVC: UIViewController {
-
+class SerachVC: UIViewController,UISearchBarDelegate,UITableViewDataSource,UITableViewDelegate {
+    
+    //Instance vars
+    var searchBar : UISearchBar!
+    
+    
+    //Outlets
+    @IBOutlet var tableView: UITableView!
+    
+    @IBOutlet var waitView: UIView!
+    
+    //methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        println("hilaw")
+        self.title! = "Places"
+        
+        self.searchBar = UISearchBar(frame: CGRectMake(0, 0, 0.9*self.view.frame.size.width, 50))
+        
+        self.searchBar.delegate = self
+        
+        self.searchBar.placeholder = "Search for places nearby Sushi,Pizza"
+        
+        self.tableView.tableHeaderView = searchBar
+        
+       // self.tableView.hidden = true
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -22,7 +43,70 @@ class SerachVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-
+    
+    
+    override  func viewWillAppear(animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        
+        
+        if let indexPath = self.tableView.indexPathForSelectedRow() {
+            
+            self.tableView.deselectRowAtIndexPath(indexPath, animated: animated)
+            
+        }
+        
+    }
+    
+    
+    //navigation
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        println("here we go")
+        
+    }
+    
+    //Search Bar delegate protocols
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        self.searchBar.resignFirstResponder()
+    }
+    
+    //tableView Delegate Protocols
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        self.searchBar.resignFirstResponder()
+    }
+    
+    //tableView data source protocols
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        
+        return 50.0
+    }
+    
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("searchResultCell", forIndexPath: indexPath) as! UITableViewCell
+        
+        cell.textLabel?.text = "Hello"
+        cell.detailTextLabel?.text = "World"
+        
+        return cell
+    }
+    
 }
 
